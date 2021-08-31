4dbaec1df6fbb5227a93fc0773f62930ce8ea0f7
Rodolfo G <rodolfo.gsilva2@senacsp.edu.br>
2021-08-31 08:45:12 -0300

    1) Na Caixa de Ferramentas do Visual Studio, arraste um componente Label para o formulário HelloWorld.
    2) Modifique o valor da propriedade Name, do Label, para Lbl_Titulo.
    
    3) No componente Form, mude o valor da propriedade Name para Frm_HelloWorld.
    
    4) Renomeie o formulário para Frm_HelloWorld.cs.
    
    5) Voltando ao componente Lbl_Titulo, mude o valor da propriedade Text para Visual Studio .NET Version.
    
    6) Mude a posição do componente Lbl_Titulo, modificando o valor da propriedade Location e para 30; 30.
    
    7) Altere o valor da propriedade Font, de Lbl_Titulo, para Arial; 12pt; style=Bold.
    
    8) No formulário Frm_HelloWorld, ajuste a propriedade Size para 428; 339.
    
    9) Salve e execute o projeto. Você terá:
    10) Da Caixa de Ferramentas, arraste o componente Button para dentro do formulário Frm_HelloWorld.
    
    11) Altere o valor da propriedade Name, do componente Button, para Btn_Sair.
    
    12) Altere o valor da propriedade Text, de Btn_Sair, para Fechar a aplicação.
    
    13) Ajuste o tamanho do botão para que o texto todo seja apresentado. Um tamanho sugerido seria mudar o valor da propriedade Size para 150; 23.
    
    14) Ajuste a posição do botão, mudando o valor da propriedade Location para 250; 265.
    
    15) Salve e execute a aplicação. Você terá:
    16) Clique no botão. Depois, na caixa Propriedades, clique no botão Eventos:
    17) Na área ao lado do evento Click, efetue um duplo clique sobre ela:
    
    18) Você irá acessar o código do evento Click, do botão Btn_Sair. Digite o seguinte código:
    
    private void Btn_Sair_Click(object sender, EventArgs e)
    {
        Application.Exit();
    }
    
    19) Salve e execute a aplicação. Você terá:
    
    20) Ao clicar no botão, a aplicação é encerrada.
    
    21) Inclua mais um botão no formulário, com as seguintes propriedades:
    
    Name: Btn_ModificaLabel
    Location: 34; 52
    Size: 209; 23
    Text: Modifica Texto do Label
    22) Na área de Designer do formulário, efetue um duplo clique sobre o botão. Automaticamente você será direcionado para a programação das ações do evento Click.
    
    23) Digite o código abaixo:
    
    private void Btn_ModificaLabel_Click(object sender, EventArgs e)
    {
        Lbl_Titulo.Text = "Label Modificado";
    }
    24) Salve e execute a aplicação. Você terá:
    
    25) Ao clicar no botão, o Label é modificado:
    
    26) Volte ao projeto e arraste um componente TextBox para o formulário Frm_HelloWorld.
    
    27) Inclua as seguintes propriedades:
    
    Name: Txt_ConteudoLabel
    Location: 34; 81
    Size: 311; 20
    28) Volte ao evento Click do botão Btn_ModificaLabel. Altere o código para:
    
    private void Btn_ModificaLabel_Click(object sender, EventArgs e)
    {
        Lbl_Titulo.Text = Txt_ConteudoLabel.Text;
    }COPIAR CÓDIGO
    29) Salve e execute a aplicação:
    
    30) Digite algum conteúdo em Txt_ConteudoLabel e depois clique no botão Btn_ModificaLabel:

---
+++ cursowf/Frm_HelloWorld.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace cursowf
+{
+    partial class Frm_HelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.Btn_Sair = new System.Windows.Forms.Button();
+            this.Btn_ModifcaLabel = new System.Windows.Forms.Button();
+            this.Txt_ConteudoLabel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Titulo.Location = new System.Drawing.Point(30, 30);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(255, 24);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Visual Studio .Net Version";
+            // 
+            // Btn_Sair
+            // 
+            this.Btn_Sair.Location = new System.Drawing.Point(250, 265);
+            this.Btn_Sair.Name = "Btn_Sair";
+            this.Btn_Sair.Size = new System.Drawing.Size(150, 23);
+            this.Btn_Sair.TabIndex = 1;
+            this.Btn_Sair.Text = "Fechar a aplicação";
+            this.Btn_Sair.UseVisualStyleBackColor = true;
+            this.Btn_Sair.Click += new System.EventHandler(this.Btn_Sair_Click);
+            // 
+            // Btn_ModifcaLabel
+            // 
+            this.Btn_ModifcaLabel.Location = new System.Drawing.Point(34, 52);
+            this.Btn_ModifcaLabel.Name = "Btn_ModifcaLabel";
+            this.Btn_ModifcaLabel.Size = new System.Drawing.Size(209, 23);
+            this.Btn_ModifcaLabel.TabIndex = 2;
+            this.Btn_ModifcaLabel.Text = "Modica Texto do Label";
+            this.Btn_ModifcaLabel.UseVisualStyleBackColor = true;
+            this.Btn_ModifcaLabel.Click += new System.EventHandler(this.Btn_ModifcaLabel_Click);
+            // 
+            // Txt_ConteudoLabel
+            // 
+            this.Txt_ConteudoLabel.Location = new System.Drawing.Point(34, 81);
+            this.Txt_ConteudoLabel.Name = "Txt_ConteudoLabel";
+            this.Txt_ConteudoLabel.Size = new System.Drawing.Size(311, 22);
+            this.Txt_ConteudoLabel.TabIndex = 3;
+           
+            // 
+            // Frm_HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 292);
+            this.Controls.Add(this.Txt_ConteudoLabel);
+            this.Controls.Add(this.Btn_ModifcaLabel);
+            this.Controls.Add(this.Btn_Sair);
+            this.Controls.Add(this.lbl_Titulo);
+            this.Name = "Frm_HelloWorld";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Titulo;
+        private System.Windows.Forms.Button Btn_Sair;
+        private System.Windows.Forms.Button Btn_ModifcaLabel;
+        private System.Windows.Forms.TextBox Txt_ConteudoLabel;
+    }
+}
+
